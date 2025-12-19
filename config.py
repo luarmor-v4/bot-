72cfe1e153da2cfb879b612cb07faa9e104197ae
luarmor-v4 <semee816@gmail.com>
2025-12-20 02:24:41 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# config.py - ISI DENGAN DATA KAMU
+
+# 1. DISCORD BOT TOKEN
+DISCORD_TOKEN = "ISI_DISINI"
+
+# 2. DISCORD ADMIN ID (ID kamu)
+ADMIN_IDS = [
+    1307489983359357019,  # Tanpa tanda kutip karena angka
+    1451468291649306635
+]
+
+# 3. WORK.INK API KEY
+WORKINK_API_KEY = "c87dbbe7-d049-483b-919e-7d678efd1ad7"
+
+# 4. WORK.INK LINK
+WORKINK_LINK = "https://work.ink/29pu/key-sistem-3"
+
+# 5. WORK.INK LINK ID
+WORKINK_LINK_ID = "29pu/key-sistem-3"
+
+# 6. GITHUB TOKEN
+GITHUB_TOKEN = "ghp_zf3EdnSjshsvsv"
+
+# 7. GITHUB REPO
+GITHUB_REPO = "https://github.com/luarmor-v4/bot-"
